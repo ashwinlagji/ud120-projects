4c168f16ff3f36ca472c84ffda1e26f97b6e4417
ashwinlagji <ashwinlagji@gmail.com>
2018-11-22 23:00:07 +0530

    K nearest neighbour algorithm test

--- K_nearest_neighbour/your_algorithm.py
+++ K_nearest_neighbour/your_algorithm.py
@@ -31,12 +31,18 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import accuracy_score
 
+clf = KNeighborsRegressor(n_neighbors=5)
 
+clf.fit(features_train, labels_train)
 
+pred = clf.predict(features_test)
 
+print len(pred), len(labels_test)
 
-
+print "Accuracy : ", clf.score(features_test, labels_test)
 
 try:
     prettyPicture(clf, features_test, labels_test)
